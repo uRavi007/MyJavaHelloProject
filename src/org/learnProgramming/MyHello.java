017a62f30d4cac7b6d5da7a6dfb58496be53015f
uRavi007 <mr.ravi007@gmail.com>
2018-11-25 19:48:48 +0000

    My second commit added new sys out print line

--- src/org/learnProgramming/MyHello.java
+++ src/org/learnProgramming/MyHello.java
@@ -3,5 +3,6 @@ package org.learnProgramming;
 public class MyHello {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
+        System.out.println("My Java Hello World");
     }
 }
